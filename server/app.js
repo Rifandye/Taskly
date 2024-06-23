433570bc44a6031bdd745e109c501592e08cdde9
Rifandye <rilfransyah@gmail.com>
2024-06-23 19:56:26 +0700

    chore: setup

---
+++ server/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const path = require("node:path");
+const AutoLoad = require("@fastify/autoload");
+
+// Pass --options via CLI arguments in command to enable these options.
+const options = {};
+
+module.exports = async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
+
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
+
+module.exports.options = options;
